db55c3d99784ccac8f8300259cde39333eae69a3
asirrosa <cuentagithub@proton.me>
2023-10-26 22:08:49 +0200

    Cambios para mejorar la experiencia del usuario

--- app/src/main/java/Main/AparcamientoAdapter.java
+++ app/src/main/java/Main/AparcamientoAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -113,8 +114,4 @@ public class AparcamientoAdapter extends RecyclerView.Adapter<AparcamientoAdapte
             notifyDataSetChanged();
         }
     };
-
-
-
-
 }
--- app/src/main/java/Main/MainActivity.java
+++ app/src/main/java/Main/MainActivity.java
@@ -147,8 +147,6 @@ public class MainActivity extends AppCompatActivity {
     public void conConexion() throws CustomException {
         try {
             Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-            SubCargarConConexion cargarConConexion = new SubCargarConConexion();
-            cargarConConexion.execute();
             if (geocoder != null) {
                 List<Address> list = geocoder.getFromLocation(latitude, longitude, 1);
                 if (list.size() > 0) {
@@ -224,38 +222,6 @@ public class MainActivity extends AppCompatActivity {
             txtAlert.setVisibility(TextView.INVISIBLE);
             txtAlert.setText("Se ha guardado la ubi del aparcamiento!");
             Toast.makeText(getApplicationContext(), "GPS activado", Toast.LENGTH_SHORT).show();
-            super.onPostExecute(unused);
-        }
-
-    }
-
-    private class SubCargarConConexion extends AsyncTask<Void,Void,Void> {
-
-        //Aqui le decimos que es lo que va a hacer mientras el gps esta iniciandose
-        @Override
-        protected void onPreExecute() {
-            actualizacionesLayout(ProgressBar.VISIBLE, R.drawable.button_background_cargar, false);
-            txtAlert.setVisibility(TextView.VISIBLE);
-            //txtAlert.setText("El gps esta arrancando...");
-            super.onPreExecute();
-        }
-
-        //Este es un metodo para que espere, de esta manera esperamos hasta que el gps esta activado
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            return null;
-        }
-
-        //Despues de que el GPS se active se hace esto
-        @Override
-        protected void onPostExecute(Void unused) {
-            txtAlert.setVisibility(TextView.INVISIBLE);
-            txtAlert.setText("Se ha guardado la ubi del aparcamiento!");
             super.onPostExecute(unused);
         }
 
